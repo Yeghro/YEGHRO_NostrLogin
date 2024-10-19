7ebd154c4fcba0527906a9946f6bedb8539fa9d4
Yeghro <130201060+Yeghro@users.noreply.github.com>
2024-10-18 19:45:34 -0700

    made more wordpress reuqested modifications.  Hoping I got everything this time

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+import { fileURLToPath } from 'url';
 import path from 'path';
 import TerserPlugin from 'terser-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   entry: './src/js/nostr-login.js',
